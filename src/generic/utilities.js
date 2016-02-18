d388427b19c9b02fe757a58ef3540a2b098ec5b4
wingedrobin <wingedrobin@gmail.com>
2016-02-18 13:19:23 +0800

    Move dictionary.js into utilities.js

--- src/generic/utilities.js
+++ src/generic/utilities.js
@@ -1,6 +1,7 @@
 "use strict" ;
 
-var util = gxd.util || { } ;
+var assert	= chai.assert ;
+var util	= gxd.util || { } ;
 
 /**
  * 
@@ -56,3 +57,131 @@ util.shuffle = function( source )
 	
 	return source ;
 } ;
+
+/**
+ * @class
+ * @extends	cc.Class
+ */
+util.Dictionary = cc.Class.extend(
+/** @lends gxd.util.Dictionary */
+{
+	/**
+	 * @param	{object}
+	 */
+	ctor : function( value )
+	{
+		for( var key in value )
+		{
+			if( typeof value[ key ] !== "function" )
+				this[ key ] = value[ key ] ;
+		}
+	} ,
+	
+	/**
+	 *
+	 */
+	free : function( )
+	{
+		this.clean( ) ;
+	} ,
+	
+	/**
+	 * @param	{string}	key
+	 * @param	{object}	value
+	 */
+	add : function( key , value )
+	{
+		assert.isString( key ) ;
+		assert.notOk( this.hasKey( key ) , key + " is all ready exist." ) ;
+		
+		this[ key ] = value ;
+	} ,
+	
+	/**
+	 * @param	{string}	key
+	 */
+	remove : function( key )
+	{
+		assert.isString( key ) ;
+		assert.ok( this.hasKey( key ) , key + " is not exist." ) ;
+		
+		this[ key ] = null ;
+		delete this[ key ] ;
+	} ,
+	
+	/**
+	 * @param	{string}	key
+	 * @return	{boolean}
+	 */
+	hasKey : function( key )
+	{
+		assert.isString( key ) ;
+		
+		return this.hasOwnProperty( key ) ;
+	} ,
+	
+	/**
+	 * @param	{string}	key
+	 * @return	{boolean}
+	 */
+	hasValue : function( key )
+	{
+		assert.isString( key ) ;
+		
+		return this[ key ] ? true : false ;
+	} ,
+	
+	/**
+	 * @return	{object}
+	 */
+	getValue : function( key )
+	{
+		assert.isString( key ) ;
+		// assert.ok( this.hasKey( key ) , key + " is not exist." ) ;
+		
+		return this[ key ] ;
+	} ,
+	
+	/**
+	 * @return	{array}
+	 */
+	getValues : function( )
+	{
+		var values = [ ] ;
+		
+		for( var key in this )
+		{
+			if( key === "__instanceId" )
+				continue ;
+			
+			if( typeof this[ key ] !== "function" )
+				values.push( this[ key ] ) ;
+		}
+		
+		return values ;
+	} ,
+	
+	/**
+	 * @
+	 */
+	clone : function( )
+	{
+		var copy = new util.Dictionary( ) ;
+		
+		for( var key in this )
+			copy[ key ] = this[ key ] ;
+		
+		return copy ;
+	} ,
+	
+	/**
+	 * @
+	 */
+	clean : function( )
+	{
+		for( var key in this )
+		{
+			this.remove( key ) ;
+		}
+	}
+} ) ;
